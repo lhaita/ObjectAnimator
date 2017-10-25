63e409e7c90156818ac228f9a35c841875e50cb9
liuhaoaita <1521846749@qq.com>
2017-10-25 13:47:17 +0800

    第一次提交，画圆动画

---
+++ app/src/main/java/liuhao/baway/com/shuxing/Point.java
@@ -0,0 +1,22 @@
+package liuhao.baway.com.shuxing;
+
+public class Point {
+  
+    private float x;  
+  
+    private float y;  
+  
+    public Point(float x, float y) {  
+        this.x = x;  
+        this.y = y;  
+    }  
+  
+    public float getX() {  
+        return x;  
+    }  
+  
+    public float getY() {  
+        return y;  
+    }  
+  
+} 
